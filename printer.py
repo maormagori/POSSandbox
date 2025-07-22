e1c7dc739dd3c8d7be3bab07b57e080077d52f2a
Maor Magori <maor@magori.online>
2025-07-22 23:58:53 +0300

    Made config work from any working dir

--- printer.py
+++ printer.py
@@ -1,7 +1,18 @@
 from escpos import config
 
+from pathlib import Path
+
+PROJECT_DIR = Path(__file__).parent
+CONFIG_FILE = PROJECT_DIR / 'config.yaml'
+
+
 def get_printer():
     printer_config = config.Config()
-    printer_config.load('./config.yaml')
+
+    if not CONFIG_FILE.exists():
+        raise FileNotFoundError(f"Configuration file not found: {CONFIG_FILE}")
+
+
+    printer_config.load(str(CONFIG_FILE))
 
     return printer_config.printer()
